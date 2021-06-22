489ef9988cb6bc09e4a782cd011bcf8aea27f53b
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-06-22 21:27:59 +0530

    :sparkles: Add credits component

---
+++ components/footer/credits.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import AniruddhaLogo from "../../public/aniruddha.png";
+
+export default function Credits() {
+  return (
+    <div className="w-full text-center text-sm md:text-base my-5">
+      <p>This website is designed, developed and maintained by</p>
+      <table
+        className="mx-auto cursor-pointer"
+        title="Aniruddha Chatterjee | Software Developer"
+        onClick={() => {
+          window.open("https://aniruddha.net");
+        }}
+      >
+        <td className="align-middle pr-2">
+          <Image src={AniruddhaLogo} height={40} width={40} />
+        </td>
+        <td className="align-baseline pt-2 font-mono">Aniruddha Chatterjee</td>
+      </table>
+    </div>
+  );
+}
