21cdb9ebd62d5b65ee1c2e4be75139f0d1395ccd
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-06-24 00:56:59 +0530

    :lipstick: Move meta data to _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,39 +38,6 @@ export default function index({
     <>
       <Head>
         <title>Mathletes - Douglas Memorial H.S. School</title>
-        <meta name="title" content="Mathletes - Douglas Memorial H.S. School" />
-        <meta
-          name="description"
-          content="Douglas Memorial H.S. School presents to you Mathletes, a fun filled two day inter-school event revolving around the beautiful world of maths!"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://mathletes.dmhss.org/" />
-        <meta
-          property="og:title"
-          content="Mathletes - Douglas Memorial H.S. School"
-        />
-        <meta
-          property="og:description"
-          content="Douglas Memorial H.S. School presents to you Mathletes, a fun filled two day inter-school event revolving around the beautiful world of maths!"
-        />
-        <meta
-          property="og:image"
-          content="https://billboard.srmkzilla.net/api/blog?title=Mathletes&subtitle=Douglas%20Memorial%20H.S.%20School&theme=dark&fontSize=180px"
-        />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://mathletes.dmhss.org/" />
-        <meta
-          property="twitter:title"
-          content="Mathletes - Douglas Memorial H.S. School"
-        />
-        <meta
-          property="twitter:description"
-          content="Douglas Memorial H.S. School presents to you Mathletes, a fun filled two day inter-school event revolving around the beautiful world of maths!"
-        />
-        <meta
-          property="twitter:image"
-          content="https://billboard.srmkzilla.net/api/blog?title=Mathletes&subtitle=Douglas%20Memorial%20H.S.%20School&theme=dark&fontSize=180px"
-        />
       </Head>
       <Navbar />
       <Landing />
